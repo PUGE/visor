69361a4a217c39639c1b2d565b3b598634985391
PUGE <100284685@qq.com>
2018-09-02 15:06:10 +0800

    增加日志方法

--- server.py
+++ server.py
@@ -28,6 +28,16 @@ def startProcess(ip, port, username, password, name):
   server = xmlrpc.client.Server('http://' + username + ':' + password + '@' + ip + ':' + port +'/RPC2')
   return json.dumps(server.supervisor.startProcess(name, True))
 
+@app.route('/readProcessStdoutLog/<ip>/<port>/<username>/<password>/<name>/<offset>/<length>')
+def readProcessStdoutLog(ip, port, username, password, name, offset, length):
+  server = xmlrpc.client.Server('http://' + username + ':' + password + '@' + ip + ':' + port +'/RPC2')
+  return json.dumps(server.supervisor.readProcessStdoutLog(name, offset, length))
+
+@app.route('/readProcessStderrLog/<ip>/<port>/<username>/<password>/<name>/<offset>/<length>')
+def readProcessStderrLog(ip, port, username, password, name, offset, length):
+  server = xmlrpc.client.Server('http://' + username + ':' + password + '@' + ip + ':' + port +'/RPC2')
+  return json.dumps(server.supervisor.readProcessStderrLog(name, offset, length))
+
 if __name__ == '__main__':
   print('程序正在监听5001端口!')
   app.run(host='0.0.0.0', port=5001)
